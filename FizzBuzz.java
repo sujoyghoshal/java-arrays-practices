b85c3d27093c81841a6dbf1f90e36cc3301a054a
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-03 14:13:27 +0530

    [sujoy] desc: Added FizzBuzz program to save and display results.

---
+++ FizzBuzz.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        String[] results = new String[number + 1];
+
+        // Generate FizzBuzz results
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        // Display results
+        for (int i = 0; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+    }
+}
